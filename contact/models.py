481cd8b9952a2323ede11f589acf0b53a5dec711
Brian Alexander <balexander04@gmail.com>
2018-08-28 22:24:34 -0500

    Adding keyword arg.

--- contact/models.py
+++ contact/models.py
@@ -8,7 +8,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length=50, blank=False)
     phone = models.CharField(max_length=50, blank=True, null=True)
     message = models.CharField(max_length=1000, blank=False)
-    contact_date = models.DateTimeField('contact_date', auto_now_add=True)
+    contact_date = models.DateTimeField(
+        verbose_name='contact_date',
+        auto_now_add=True
+    )
 
     def __str__(self):
         return self.name
