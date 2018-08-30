cb602125331810ce057d879b33ec4d09ce251fbf
Brian Alexander <balexander04@gmail.com>
2018-08-30 11:43:11 -0500

    Completing doc string and making message on admin page readonly.

--- contact/admin.py
+++ contact/admin.py
@@ -5,12 +5,12 @@ from .models import Contact
 class ContactAdmin(admin.ModelAdmin):
     """Admin class for Contact app
 
-        fieldsets are
-        list_display are
+        fieldsets are fields displayed on individual contact page
+        list_display are the fields displayed on the list of contacts
     """
     fieldsets = [
         (
-            "Contact",
+            'Contact',
             {
                 'fields': [
                     'name',
@@ -30,6 +30,7 @@ class ContactAdmin(admin.ModelAdmin):
         'contact_date'
     )
     list_filter = ['contact_date']
+    readonly_fields = ['message']
     search_fields = ['name']
 
 
