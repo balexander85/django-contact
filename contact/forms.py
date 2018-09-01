a5fe287cb7fb231a34fb98e17b9e202c82752f1f
Brian Alexander <balexander04@gmail.com>
2018-08-31 20:09:32 -0500

    Changing apostrophes.

--- contact/forms.py
+++ contact/forms.py
@@ -4,21 +4,21 @@ from .models import Contact
 
 class ContactForm(forms.ModelForm):
 
-    SUCCESS_MESSAGE = "Thank you for reaching out." \
-                      " We will be in touch as soon as possible."
+    SUCCESS_MESSAGE = 'Thank you for reaching out.' \
+                      ' We will be in touch as soon as possible.'
 
     class Meta:
         model = Contact
-        fields = "__all__"
+        fields = '__all__'
 
-    # error_css_class = "error"
-    # required_css_class = "required"
+    # error_css_class = 'error'
+    # required_css_class = 'required'
 
     name = forms.CharField(
         required=True, max_length=50, widget=forms.TextInput(
             attrs={
-                "placeholder": "Name", "maxlength": "30",
-                "class": "w3-third w3-input w3-border", "type": "text"
+                'placeholder': 'Name', 'maxlength': '30',
+                'class': 'w3-third w3-input w3-border', 'type': 'text'
             }
         )
     )
@@ -26,8 +26,8 @@ class ContactForm(forms.ModelForm):
     email = forms.EmailField(
         required=True, max_length=50, widget=forms.TextInput(
             attrs={
-                "placeholder": "Email", "maxlength": "50",
-                "class": "w3-third w3-input w3-border", "type": "text"
+                'placeholder': 'Email', 'maxlength': '50',
+                'class': 'w3-third w3-input w3-border', 'type': 'text'
             }
         )
     )
@@ -35,8 +35,8 @@ class ContactForm(forms.ModelForm):
     phone = forms.CharField(
         required=False, max_length=30, widget=forms.TextInput(
             attrs={
-                "placeholder": "Phone", "maxlength": "50",
-                "class": "w3-third w3-input w3-border", "type": "text"
+                'placeholder': 'Phone', 'maxlength': '50',
+                'class': 'w3-third w3-input w3-border', 'type': 'text'
             }
         )
     )
@@ -44,20 +44,20 @@ class ContactForm(forms.ModelForm):
     message = forms.CharField(
         required=True, widget=forms.TextInput(
             attrs={
-                "placeholder": "Message", "maxlength": "1000",
-                "class": "w3-input w3-border", "type": "text"
+                'placeholder': 'Message', 'maxlength': '1000',
+                'class': 'w3-input w3-border', 'type': 'text'
             }
         )
     )
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields["name"].label = ""
-        self.fields["email"].label = ""
-        self.fields["phone"].label = ""
-        self.fields["message"].label = ""
+        self.fields['name'].label = ''
+        self.fields['email'].label = ''
+        self.fields['phone'].label = ''
+        self.fields['message'].label = ''
 
     def clean(self):
         cleaned_data = super(ContactForm, self).clean()
-        name = cleaned_data.get("name")
-        email = cleaned_data.get("email")
+        name = cleaned_data.get('name')
+        email = cleaned_data.get('email')
